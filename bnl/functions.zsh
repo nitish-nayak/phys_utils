d1ed652b164e22223d6030eb87d867d33dfbda6a
Nitish Nayak <felarof.nayak@gmail.com>
2024-05-08 09:47:38 -0400

    add function to fix corrupt zsh history

--- bnl/functions.zsh
+++ bnl/functions.zsh
@@ -5,3 +5,14 @@ function stage {
   time parallel -j 15 cp -rfvL {} "$1" ::: "${@:2}"
   echo "[Done]"
 }
+
+function fix_zshrc {
+  orig=`pwd`
+  cd ~
+  mv .zsh_history .zsh_history_bad
+  strings .zsh_history_bad > .zsh_history
+  fc -R .zsh_history
+  rm ~/.zsh_history_bad
+  echo "[Fixed!]"
+  cd "$orig"
+}
